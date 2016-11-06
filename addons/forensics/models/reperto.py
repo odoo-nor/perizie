31000bf6d0791a7d08a0eed5d7098dade681eaff
norman.argiolas <norman.argiolas@flosslab.com>
2016-11-06 03:11:08 +0100

    this closes #17 Visualizzare il numero della Perizia nella vista dell'inserimento del reperto

--- addons/forensics/models/reperto.py
+++ addons/forensics/models/reperto.py
@@ -11,6 +11,28 @@ class Reperto(models.Model):
     _description = 'Reperto'
     _order = 'name'
 
+    def _get_perizia_id(self, cr, uid, ids, context=None):
+        if 'active_id' in ids.keys():
+            return ids['active_id']
+
+    _defaults = {
+        'perizia_id': _get_perizia_id,
+    }
+
+    # @api.model
+    # def default_get(self, vals):
+    #     res = super(Reperto, self).default_get(vals)
+        # country_ids = self.env['res.country'].search([('code', '=', 'IN')])
+        # 
+        # if country_ids:
+        #     res.update({
+        #         'country_id': country_ids[0].id,  # Many2one field
+        #         'city': 'Gandhinagar',
+        #         'website': 'www.odootechnical.com',
+        #         'email': 'contact@odootechnical.com'
+        #     })
+        # return res
+
     descrizione_verbale = fields.Text(string='Descrizione come da Verbale',
                                       required=False, translate=False)
 
@@ -21,3 +43,7 @@ class Reperto(models.Model):
     immagine_ids = fields.One2many(
         'forensics.img.reperto', 'reperto_img_id',
         String='Immagini')
+
+    # def create(self, cr, uid, vals, context=None):
+    #     reperto = super(Reperto, self).create(cr, uid, vals, context=context)
+    #     return reperto
