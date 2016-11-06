bfa44d7ebf6bdfad05be902914d1b7a72385ea8f
norman.argiolas <norman.argiolas@flosslab.com>
2016-11-06 17:27:54 +0100

    this closes Creare #16 - la gestione delle foto dei reperti

--- addons/forensics/models/img_reperto.py
+++ addons/forensics/models/img_reperto.py
@@ -10,12 +10,24 @@ class ImgReperto(models.Model):
     _description = 'Immagine Reperto'
     _order = 'name'
 
+    # def _get_perizia_id(self, cr, uid, ids, context=None):
+    #     if 'active_id' in ids.keys():
+    #         return ids['active_id']
+
+    # _defaults = {
+    #     'perizia_id': _get_perizia_id,
+    # }
+
+    # @api.model
+    # def default_get(self, vals):
+    #     res = super(ImgReperto, self).default_get(vals)
+
     name = fields.Text(string='Descrizione foto reperto',
                        required=True, size=100, translate=False)
 
     image = fields.Binary("Image", attachment=True, help="Immagine limited to 1024x1024px")
 
-    reperto_img_id = fields.Many2one('forensics.reperto', 'Reperto')
+    reperto_id = fields.Many2one('forensics.reperto', 'Reperto')
 
     perizia_id = fields.Many2one('forensics.perizia', 'Perizia',
                                  compute='_compute_perizia',
@@ -24,9 +36,14 @@ class ImgReperto(models.Model):
                                  compute_sudo=False,
                                  )
 
-    @api.depends('reperto_img_id')
+    @api.depends('reperto_id')
     def _compute_perizia(self):
-        pass
+        for img_reperto in self.filtered('name'):
+            self.perizia_id = img_reperto.reperto_id.perizia_id
 
     def _inverse_perizia(self):
         pass
+
+    # def create(self, cr, uid, vals, context=None):
+    #     reperto = super(ImgReperto, self).create(cr, uid, vals, context=context)
+    #     return reperto
