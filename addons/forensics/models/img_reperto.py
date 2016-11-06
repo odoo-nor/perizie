1a2b0c4f87ef8c2bdf6002cf04427c8078eefcc1
norman.argiolas <norman.argiolas@flosslab.com>
2016-11-06 01:31:59 +0100

    this closes #16 - Creare la gestione delle foto dei reperti

--- addons/forensics/models/img_reperto.py
+++ addons/forensics/models/img_reperto.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields
+from openerp import api
 
 
 class ImgReperto(models.Model):
@@ -15,3 +16,17 @@ class ImgReperto(models.Model):
     image = fields.Binary("Image", attachment=True, help="Immagine limited to 1024x1024px")
 
     reperto_img_id = fields.Many2one('forensics.reperto', 'Reperto')
+
+    perizia_id = fields.Many2one('forensics.perizia', 'Perizia',
+                                 compute='_compute_perizia',
+                                 inverse='_inverse_perizia',
+                                 store=False,
+                                 compute_sudo=False,
+                                 )
+
+    @api.depends('reperto_img_id')
+    def _compute_perizia(self):
+        pass
+
+    def _inverse_perizia(self):
+        pass
