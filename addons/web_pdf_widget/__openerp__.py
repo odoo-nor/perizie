5827fa4558381626627672e4c8919631da416f39
norman.argiolas <norman.argiolas@flosslab.com>
2016-11-19 01:52:20 +0100

    prove per i report

---
+++ addons/web_pdf_widget/__openerp__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Pdf Viewer Widget',
+    'description': '''
+This module add a pdf preview to binary data
+
+This module works with OpenERP 9.0.
+''',
+    'version': '7.0.1',
+    'author': 'Innoviu Srl',
+    'category': 'Usability',
+    'website': 'https://www.example.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'web',
+        ],
+    'python': ['magic'],
+    'data': ['base_data.xml'],
+    'js': ['static/src/js/web_pdf_widget.js'],
+    'qweb': ['static/src/xml/web_pdf_widget.xml'],
+    'installable': True,
+    'auto_install': False,
+}
